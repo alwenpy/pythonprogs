6b0c293ae74a41dafba51477c28e38efca4c9ecb
Arvind Srivastav <94129388+alwenpy@users.noreply.github.com>
2023-02-25 20:36:23 +0530

    Update Weird not weird.py

--- Weird not weird.py
+++ Weird not weird.py
@@ -3,8 +3,8 @@
 num=int(input("Number daalo"))
 
 if num %2 != 0 or num in range(6,20):
-    print("Weird")
+ print("Weird")
     
-elif num % 2 == 0  or num in range(2,5) or num> 20:
+elif num % 2 = 0  or num in range(2,5) or num> 20:
     print("Not Weird")
    
